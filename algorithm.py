a75a0a28762757c5b769810e54557da9324bd705
CHOI <jack96318@hanmail.net>
2022-01-16 13:39:43 +0900

    1/16 - 입출력과 사칙연산

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,23 @@
-print("Hello World!")
+print("\    /\\\n" + " )  ( \')\n" + "(  /  )\n" + " \(__)|\n")
+print("|\_/|\n" + "|q p|   /}\n" + "( 0 )\"\"\"\\\n" + "|\"^\"`    |\n" + "||_/=\\\\__|")
 
-print("강한친구 대한육군\n강한친구 대한육군")
+a, b = input().split()
+print(int(a) + int(b))
+print(int(a) - int(b))
+print(int(a) * int(b))
+print(int(int(a) / int(b)))
+print(int(a) % int(b))
+
+A, B, C = input().split()
+print((int(A) + int(B))%int(C))
+print(((int(A) % int(C)) + (int(B) % int(C))) % int(C))
+print((int(A) * int(B))%int(C))
+print(((int(A) % int(C)) * (int(B) % int(C))) % int(C))
+
+A = int(input())
+B = input()
+print(int(B[2]) * A)
+print(int(B[1]) * A)
+print(int(B[0]) * A)
+print((int(B[0]) * A)*100 + (int(B[1]) * A) * 10 + (int(B[2]) * A))
+
