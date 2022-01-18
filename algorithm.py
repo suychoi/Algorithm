98b61f4cfb3db48bc636a722c1405e775ca6f40c
CHOI <jack96318@hanmail.net>
2022-01-19 01:25:31 +0900

    1/19 - if 문

--- algorithm.py
+++ algorithm.py
@@ -29,16 +29,27 @@
 # else:
 #     print("0")
 
-a = int(input())
-b = int(input())
-if a < 0 and b < 0:
-    print("3")
-elif a > 0 and b > 0:
-    print("1")
-elif a < 0 and b > 0:
-    print("2")
-elif a > 0 and b < 0:
-    print("4")
+# a = int(input())
+# b = int(input())
+# if a < 0 and b < 0:
+#     print("3")
+# elif a > 0 and b > 0:
+#     print("1")
+# elif a < 0 and b > 0:
+#     print("2")
+# elif a > 0 and b < 0:
+#     print("4")
+
+H, M = input().split()
+H = int(H)
+M = int(M)
+if M >= 45:
+    print(H + " " + (M-45))
+elif M < 45 and H == 0:
+    print(H+23 + " " + (M + 15))
+elif M < 45:
+    print((H-1) + " " + (M + 15))
+
 
 
 
