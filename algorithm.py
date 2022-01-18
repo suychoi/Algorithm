a029fdb1db5aa0193bf639a2a83be97421f60267
CHOI <jack96318@hanmail.net>
2022-01-19 01:06:38 +0900

    1/19 - if 문

--- algorithm.py
+++ algorithm.py
@@ -21,10 +21,34 @@
 #
 # print('FFFFFFDCBAA'[int(input())//10])
 
+# a = int(input())
+# if a % 4 == 0 and a % 100 != 0:
+#     print("1")
+# elif a % 400 == 0:
+#     print("1")
+# else:
+#     print("0")
+
 a = int(input())
-if a % 4 == 0 and a % 100 != 0:
+b = int(input())
+if a < 0 and b < 0:
+    print("3")
+elif a > 0 and b > 0:
     print("1")
-elif a % 400 == 0:
-    print("1")
-else:
-    print("0")
+elif a < 0 and b > 0:
+    print("2")
+elif a > 0 and b < 0:
+    print("4")
+
+
+
+
+
+
+
+
+
+
+
+
+
