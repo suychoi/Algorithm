0c545e5f29e89d15f7cab4c6177d5a8033279f10
CHOI <jack96318@hanmail.net>
2022-01-25 20:33:29 +0900

    1/25 - while

--- algorithm.py
+++ algorithm.py
@@ -1,5 +1,3 @@
-import sys
-
 # a, b = input().split()
 # if int(a) > int(b):
 #     print(">")
@@ -7,8 +5,8 @@ import sys
 #     print("<")
 # elif int(a) == int(b):
 #     print("==")
-
-# 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+#
+# # 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
 # a = int(input())
 # if 90 <= a <= 100:
 #     print("A")
@@ -22,7 +20,7 @@ import sys
 #     print("F")
 #
 # print('FFFFFFDCBAA'[int(input())//10])
-
+#
 # a = int(input())
 # if a % 4 == 0 and a % 100 != 0:
 #     print("1")
@@ -30,7 +28,7 @@ import sys
 #     print("1")
 # else:
 #     print("0")
-
+#
 # a = int(input())
 # b = int(input())
 # if a < 0 and b < 0:
@@ -41,7 +39,7 @@ import sys
 #     print("2")
 # elif a > 0 and b < 0:
 #     print("4")
-
+#
 # H, M = input().split()
 # if int(M) >= 45:
 #     print(H,str(int(M)-45))
@@ -52,38 +50,107 @@ import sys
 #
 # a, b = map(int, input().split())
 # print((a - (b < 45)) % 24, (b - 45) % 60)
-
+#
 # n = int(input())
 # for x in range(9):
 #     print( str(n) + " * " + str((x+1)) + " = " + str(int(n * (x+1))))
-
+#
 # n = int(input())
 # for x in range(n):
 #     a, b = input().split()
 #     print(int(a) + int(b))
-
+#
 # n = int(input())
 # y = 0
 # for x in range(n):
 #     y = y + (x+1)
 # print(y)
-
-
+#
+#
 #
 # n = int(input())
 # for x in range(n):
 #     a, b = map(int, sys.stdin.readline().split())   #반복문으로 여러 번 입력 받을 때
 #     print(a + b)
-
+#
 # n = int(input())
 # for x in range(n):
 #     print(n-x)
+#
+# import sys
+# T = int(input())
+# for x in range(T):
+#     a, b = map(int, sys.stdin.readline().split())
+#     print("Case #" + str(int(x)+1)+ ": " + str(a) + " + " + str(b) + " = " + str(a + b))
+#
+# T = int(input())
+# for x in range(T):
+#     print("*" * (x+1))
+#
 
-import sys
-T = int(input())
-for x in range(T):
-    a, b = map(int, sys.stdin.readline().split())
-    print("Case #" + str(int(x)+1)+ ": " + str(a) + " + " + str(b) + " = " + str(a + b))
+# N = int(input())
+# for x in range(N):
+#     print((" " * (N - (x+1))) + ("*" * (x+1)))
+
+# N, X = input().split()
+# A = list(map(int, input().split()))
+# for x in A:
+#     if x < int(X):
+#         print(x)
+
+## While
+
+# import sys
+# while True:
+#     try:
+#         a, b = map(int, sys.stdin.readline().split())
+#         print(a + b)
+#     except:
+#         break
+
+# N = int(input())
+# T = N
+# C = 0
+# while True:
+#     C = C+1
+#     if N < 10:
+#         N = int("0" + str(N))
+#
+#     a = N//10
+#     b = N%10
+#     c = (a + b)
+#     M = (b * 10) + (c%10)
+#
+#     if M == T:
+#         print(C)
+#         break
+#     else:
+#         N = M
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
