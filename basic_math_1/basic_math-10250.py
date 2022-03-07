be365efff0a3faf2aa3f5114f5f17eca11d62697
CHOI <jack96318@hanmail.net>
2022-03-07 23:49:29 +0900

    10250

--- basic_math_1/basic_math-10250.py
+++ basic_math_1/basic_math-10250.py
@@ -1,14 +1,17 @@
 # ------------------------------------------------------------------------------------ 10250 번
-# 풀이시간
+# 풀이시간 24시간...
 
 Count = int(input())
-Z = []
 
-for i in range(0, Count):
+for i in range(Count):
     H, W, N = map(int, input().split())
-    Z.append([H, W, N])
-
-
+    if N % H == 0:
+        a = H * 100     #층수
+        b = N //H  #호수
+    else:
+        a = (N % H) * 100
+        b =  1 + N //H
+    print(a + b)
 
 
 
