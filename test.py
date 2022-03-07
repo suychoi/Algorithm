d9b7e5244d39ea99c8daba18b4b0fa48a0c8f4ac
CHOI <jack96318@hanmail.net>
2022-03-07 18:48:24 +0900

    10250

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
-
-
-for i in range (2, 10):
-    print(i)
+t = int(input())
+for i in range(t):
+    h, w, n = map(int, input().split())
+    f = 0
+    ho = 0
+    if n % h == 0:
+        f = h * 100
+        ho = n // h
+    else:
+        f = (n % h) * 100
+        ho = 1 + n // h
+    print(f + ho)
 
 # if new_word.count(word[index]) >
