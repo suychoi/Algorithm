16707186c5759f3bc649035b1c80aa4e598fa2e4
CHOI <jack96318@hanmail.net>
2022-03-10 00:26:29 +0900

    1978

--- basic_math2/basic_math-1978.py
+++ basic_math2/basic_math-1978.py
@@ -4,19 +4,19 @@
 
 N = int(input())
 number = map(int, input().split())
+count = 0
 
-def sosu():
-    count = 0
-    for num in number:
-        error = 0
-        if num > 1:
-          if num == 2:
-              count += 1
-          for i in range(3, num-1):
-              if num % i == 0:
-                error += 1
-                break
-          if error == 0:
-              count += 1
-    return print(count)
-sosu()
+for num in number:
+    error = 0
+    if num == 1:
+        continue
+    if num == 2:
+        count += 1
+        continue
+    for i in range(2, num):
+      if num % i == 0:
+        error += 1
+        break
+    if error == 0:
+        count += 1
+print(count)
