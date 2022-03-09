64babbfaffd557fb4210d3894344c1119fda170e
CHOI <jack96318@hanmail.net>
2022-03-10 01:12:05 +0900

    11653

--- test.py
+++ test.py
@@ -1,14 +1,5 @@
-t = int(input())
-for i in range(t):
-    h, w, n = map(int, input().split())
-    f = 0
-    ho = 0
-    if n % h == 0:
-        f = h * 100
-        ho = n // h
-    else:
-        f = (n % h) * 100
-        ho = 1 + n // h
-    print(f + ho)
+N = 20
+N //= 2
+print(N)
 
 # if new_word.count(word[index]) >
