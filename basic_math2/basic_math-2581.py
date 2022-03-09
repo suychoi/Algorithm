771ebed01dcf9823911ca359881a5b7b9d6dae66
CHOI <jack96318@hanmail.net>
2022-03-10 00:46:57 +0900

    2581

---
+++ basic_math2/basic_math-2581.py
@@ -0,0 +1,28 @@
+# ------------------------------------------------------------------------------------ 2581 번
+# 풀이시간 00:35~
+
+
+M = int(input())
+N = int(input())
+num_list = []
+for i in range(M, N+1):
+    error = 0
+    if i == 1:
+        continue
+    elif i == 2:
+        num_list.append(i)
+        continue
+    for z in range(2, i):
+        if i % z == 0:
+            error += 1
+            break
+    if error == 0:
+        num_list.append(i)
+
+if len(num_list) > 0:
+    print(sum(num_list))
+    print(num_list[0])
+else:
+    print(-1)
+
+
