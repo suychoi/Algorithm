85865c206ca9dc222529650b5d4ffd8ace80c038
suychoi <jack96318@hanmail.net>
2022-06-04 16:13:41 +0900

    10872

---
+++ recursion/10872.py
@@ -0,0 +1,12 @@
+"""
+0보다 크거나 같은 정수 N이 주어진다. 이때, N!을 출력하는 프로그램을 작성하시오.
+"""
+import sys
+N = int(sys.stdin.readline())
+K = 1
+if N == 0:
+    print(1)
+else:
+    for i in range(1, N+1):
+        K *= i
+    print(K)
