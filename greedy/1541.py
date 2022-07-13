a1be599eea817d099d8edb3b0a0610672454d8af
CHOI SU YEOL <jack96318@hanmail.net>
2022-07-13 23:45:03 +0900

    1541 번

---
+++ greedy/1541.py
@@ -0,0 +1,24 @@
+"""
+세준이는 양수와 +, -, 그리고 괄호를 가지고 식을 만들었다.
+
+그리고 나서 세준이는 괄호를 모두 지웠다.
+
+그리고 나서 세준이는 괄호를 적절히 쳐서 이 식의 값을 최소로 만들려고 한다.
+
+괄호를 적절히 쳐서 이 식의 값을 최소로 만드는 프로그램을 작성하시오.
+"""
+# 문자와 기호로 분류 - 가 붙을때 다음 - 가 나오기 전까지 괄호로 묶는다.
+
+formula = input().split('-')
+
+result = 0
+for num in range(len(formula)):
+    f1 = formula[num].lstrip('0')
+    f1 = f1.strip()
+    if num == 0:
+        result = eval(f1)
+    elif formula[num]:
+        result -= eval(f1)
+
+print(result)
+
